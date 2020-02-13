fbe4ce31b05830a2f3ef3fe485b60ffe79b43a51
Anastasia Videneeva <nastya.videneeva@gmail.com>
2020-02-13 12:08:27 -0500

    Adds jQuery effects

--- src/js/controls.js
+++ src/js/controls.js
@@ -1,8 +1,10 @@
-$('.controls__generate-btn').click(() => {
+$(document).ready(function(){
+  $(".loading").fadeOut("slow");
+  $(".controls-wrapper").hide();
+
+  $('.controls__generate-btn').click(() => {
+    $('.controls-wrapper').slideDown("slow");
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     drawRandomTree(390, 550, 120, 0, 10);
-});
-
-$(document).ready(function(){
-  $(".loading").hide();
+  });
 });
