11a356faaf27cbcf88b6ed44d20ec7817ec537f8
Anastasia Videneeva <nastya.videneeva@gmail.com>
2020-02-14 18:15:05 -0500

    Refactoring: splits tree generation, drawing and jQuery

--- src/js/controls.js
+++ src/js/controls.js
@@ -22,3 +22,41 @@ $(document).ready(() => {
   $( "#angle" ).val( $( "#slider-range" ).slider( "values", 0 ) +
     " deg - " + $( "#slider-range" ).slider( "values", 1 ) + " deg");
 });
+
+// PARAMS
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let currentTree = [];
+
+const START_X = 390;
+const START_Y = 550;
+const START_LENGTH = 120;
+const START_ANGLE = 0;
+const START_WIDTH = 10;
+const LEAVES_WIDTH = 2;
+
+// buttons
+$('.controls__btn--generate').click(() => {
+  $('.controls-wrapper').slideDown("slow");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  currentTree = [];
+  const leavesColor = $("#leaves-color").val();
+  const branchesColor = $("#branch-color").val();
+  const minAngle = $( "#slider-range" ).slider( "values", 0 );
+  const maxAngle = $( "#slider-range" ).slider( "values", 1 );
+  // TODO: get new angle
+  generateRandomTree(currentTree, START_X, START_Y, START_LENGTH, START_ANGLE, START_WIDTH, minAngle, maxAngle);
+  drawTree(ctx, currentTree, LEAVES_WIDTH, leavesColor, branchesColor);
+  });
+
+$('.controls__btn--clear').click(() => {
+  currentTree = [];
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+const saveBtn = $('.controls__btn--save');
+saveBtn.click(() => {
+  saveBtn.attr("href", canvas.toDataURL());
+  saveBtn.attr("download", "my-tree.png");
+});
--- src/js/script.js
+++ src/js/script.js
@@ -1,102 +1,62 @@
-$(document).ready(() => {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
+const drawLine = (ctx, x0, y0, x1, y1, color, width = 1) => {
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.moveTo(x0, y0);
+    ctx.lineTo(x1, y1);
+    ctx.strokeStyle = color;
+    ctx.stroke();
+}
 
-    const drawLine = (x0, y0, x1, y1, width = 1) => {
-        ctx.beginPath();
-        ctx.lineWidth = width;
-        ctx.moveTo(x0, y0);
-        ctx.lineTo(x1, y1);
-        if (width < 2) {
-            // ctx.strokeStyle = "#469105";
-            ctx.strokeStyle = $("#leaves-color").val();
-        } else {
-            ctx.strokeStyle = ctx.strokeStyle = $("#branch-color").val();;
+const getRandomLenCoef = (min = 0.40, max = 0.95) => {
+    return Math.random() * (max - min) + min;
+}
+
+const getRandomAngle = (minDeg, maxDeg) => {
+    const minRad = minDeg * Math.PI / 180;
+    const maxRad = maxDeg * Math.PI / 180;
+    return Math.random() * (maxRad - minRad) + minRad;
+}
+
+const PADDING = 15;
+
+const generateRandomTree = (branches, x0, y0, len, angle, width, minAngle, maxAngle) => {
+    const x1 = x0 + Math.sin(angle) * len;
+    const y1 = y0 - Math.cos(angle) * len;
+
+    const newWidth = width * 0.75 > 1 ? width * 0.75 : 1;
+
+    if (len > 3 && (x1 > PADDING) && (y1 > PADDING)
+            && (x1 < canvas.width - PADDING) && (y1 < canvas.height - PADDING)) {
+        branches.push({x0, y0, x1, y1, width});
+        generateRandomTree(branches, x1, y1, len * getRandomLenCoef(), angle + getRandomAngle(minAngle, maxAngle), newWidth, minAngle, maxAngle);
+        generateRandomTree(branches, x1, y1, len * getRandomLenCoef(), angle - getRandomAngle(minAngle, maxAngle), newWidth, minAngle, maxAngle);
+        if (Math.random() > 0.7) {
+            generateRandomTree(branches, x1, y1, len * getRandomLenCoef(), (angle - getRandomAngle(minAngle, maxAngle)) / 2, newWidth, minAngle, maxAngle);
         }
-        ctx.stroke();
-    }
-
-    const TURN_ANGLE_DEGREES = 35;
-    const TURN_ANGLE_RAD = TURN_ANGLE_DEGREES * Math.PI / 180;
-
-    const drawTree = (x0, y0, len, angle, width) => {
-        const x1 = x0 + Math.sin(angle) * len;
-        const y1 = y0 - Math.cos(angle) * len;
-        drawLine(x0, y0, x1, y1, width);
-
-        const newLen = len * 0.75;
-        const newWidth = width * 0.75 > 1 ? width * 0.75 : 1;
-        if (newLen > 3) {
-            drawTree(x1, y1, newLen, angle + TURN_ANGLE_RAD, newWidth);
-            drawTree(x1, y1, newLen, angle - TURN_ANGLE_RAD, newWidth);
-        }
-    }
-
-    // drawTree(390, 550, 120, 0, 10);
-
-    const getRandomLenCoef = (min = 0.40, max = 0.95) => {
-        return Math.random() * (max - min) + min;
-    }
-
-    // const getRandomAngle = (min = MIN_ANGLE * Math.PI / 180, max = MAX_ANGLE * Math.PI / 180) => {
-    //     return Math.random() * (max - min) + min;
-    // };
-
-    const getRandomAngle = () => {
-        const MIN_ANGLE = $( "#slider-range" ).slider( "values", 0 );
-        const MAX_ANGLE = $( "#slider-range" ).slider( "values", 1 );
-        const min = MIN_ANGLE * Math.PI / 180;
-        const max = MAX_ANGLE * Math.PI / 180;
-        return Math.random() * (max - min) + min;
-    }
-
-    const PADDING = 15;
-
-    const drawRandomTree = (x0, y0, len, angle, width) => {
-        const x1 = x0 + Math.sin(angle) * len;
-        const y1 = y0 - Math.cos(angle) * len;
-
-        const newWidth = width * 0.75 > 1 ? width * 0.75 : 1;
-
-        if (len > 3 && (x1 > PADDING) && (y1 > PADDING)
-             && (x1 < canvas.width - PADDING) && (y1 < canvas.height - PADDING)) {
-            drawLine(x0, y0, x1, y1, width);
-            drawRandomTree(x1, y1, len * getRandomLenCoef(), angle + getRandomAngle(), newWidth);
-            drawRandomTree(x1, y1, len * getRandomLenCoef(), angle - getRandomAngle(), newWidth);
-            if (Math.random() > 0.7) {
-                drawRandomTree(x1, y1, len * getRandomLenCoef(), (angle - getRandomAngle()) / 2, newWidth);
-            }
-            if (Math.random() > 0.7) {
-                drawRandomTree(x1, y1, len * getRandomLenCoef(), (angle + getRandomAngle()) / 2, newWidth);
-            }
+        if (Math.random() > 0.7) {
+            generateRandomTree(branches, x1, y1, len * getRandomLenCoef(), (angle + getRandomAngle(minAngle, maxAngle)) / 2, newWidth, minAngle, maxAngle);
         }
     }
+}
 
-    $('.controls__btn--generate').click(() => {
-        $('.controls-wrapper').slideDown("slow");
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawRandomTree(390, 550, 120, 0, 10);
-      });
+const drawTree = (ctx, tree, leavesWidth, leavesColor, branchesColor) => {
+    tree.map((branch) => drawLine(ctx, branch.x0, branch.y0, branch.x1, branch.y1, branch.width > leavesWidth ? branchesColor : leavesColor, branch.width));
+}
 
-    $('.controls__btn--clear').click(() => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    });
+// not a random tree
+// const TURN_ANGLE_DEGREES = 35;
+// const TURN_ANGLE_RAD = TURN_ANGLE_DEGREES * Math.PI / 180;
+// const drawNotTree = (x0, y0, len, angle, width) => {
+//     const x1 = x0 + Math.sin(angle) * len;
+//     const y1 = y0 - Math.cos(angle) * len;
+//     drawLine(x0, y0, x1, y1, width);
 
-    const saveBtn = $('.controls__btn--save');
-    saveBtn.click(() => {
-        saveBtn.attr("href", canvas.toDataURL());
-        saveBtn.attr("download", "my-tree.png");
-    });
-
-    const getParamsDrawTree = () => {
-        // get interface params
-        // generate tree and store in an array
-        // draw tree
-        // ??? mb this function needs to receive callback as an argument - hide "loading" spinner
-    }
-});
-
-//drawRandomTree(390, 550, 120, 0, 10);
-
-
+//     const newLen = len * 0.75;
+//     const newWidth = width * 0.75 > 1 ? width * 0.75 : 1;
+//     if (newLen > 3) {
+//         drawTree(x1, y1, newLen, angle + TURN_ANGLE_RAD, newWidth);
+//         drawTree(x1, y1, newLen, angle - TURN_ANGLE_RAD, newWidth);
+//     }
+// }
+// drawTree(390, 550, 120, 0, 10);
 
